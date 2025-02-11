4ed70f6fce3d365404b4ab899f91435e22c6dc66
Yury Golub <yuragolub21@gmail.com>
2025-02-11 15:30:15 +0300

    Add hrsize function.

---
+++ AppExtensionsLib/Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace AppExtensionsLib;
+
+public static class Helper
+{
+    public static string HRSize(long size, string format = "F1", string postfix = "B")
+    {
+        double current = size;
+        const int KiBSize = 1024;
+        const int MiBSize = KiBSize * KiBSize;
+        const int GiBSize = MiBSize * KiBSize;
+
+        return size switch
+        {
+            < KiBSize => $"{size} {postfix}",
+            < MiBSize => $"{(current / KiBSize).ToString(format)} Ki{postfix}",
+            < GiBSize => $"{(current / MiBSize).ToString(format)} Mi{postfix}",
+            _ => $"{(current / GiBSize).ToString(format)} Gi{postfix}",
+        };
+    }
+}
